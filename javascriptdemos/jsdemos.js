18232a9775ceb773c3111d808c5527f118c28ca1
praveen <180843326+PPraveenKumar-klu@users.noreply.github.com>
2025-08-22 10:15:55 +0530

    added folder javascriptdemos

---
+++ javascriptdemos/jsdemos.js
@@ -0,0 +1,16 @@
+document.writeln("This is from js <br>");
+
+
+document.writeln("The value of x is: " + x+"<br>");
+
+let y = 200;
+document.writeln("value "+(x+y)+"<br>");
+{
+    // let x =200;
+    document.writeln("The value of x is: " + x+ "<br>");
+}
+
+
+document.writeln("The value of x is: " + x+ "<br>");
+
+var x = 100; //hoisting;
