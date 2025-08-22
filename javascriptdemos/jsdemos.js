f99e8047b0fde40f42decc3fed2f2dd9b1a02776
praveen <180843326+PPraveenKumar-klu@users.noreply.github.com>
2025-08-22 13:07:49 +0530

    updated jsdemos.js

--- javascriptdemos/jsdemos.js
+++ javascriptdemos/jsdemos.js
@@ -1,16 +1,86 @@
-document.writeln("This is from js <br>");
+// document.writeln("This is from js <br>");
 
 
-document.writeln("The value of x is: " + x+"<br>");
+// document.writeln("The value of x is: " + x+"<br>");
 
-let y = 200;
-document.writeln("value "+(x+y)+"<br>");
-{
-    // let x =200;
-    document.writeln("The value of x is: " + x+ "<br>");
+// let y = 200;
+// document.writeln("value "+(x+y)+"<br>");
+// {
+//     // let x =200;
+//     document.writeln("The value of x is: " + x+ "<br>");
+// }
+
+
+// document.writeln("The value of x is: " + x+ "<br>");
+
+// var x = 100; //hoisting;
+
+
+// // Datatypes in Javascript
+
+// let name ="kiran";
+// document.writeln(name+" Type is. : "+ typeof name+ " <br>");
+
+// let x = 100;
+// document.writeln(x+" Type is. : "+ typeof x+ " <br>");
+
+// x = true;
+// document.writeln(x+" Type is. : "+ typeof x+ " <br>");
+
+// x = 123745789547893457895874879879487457897858795874789587954897045879457843578879457895478945879897478943578953487457887953478945387905347803457807894053879345789534879458039778590438794358793458970354879023467889723478923478234879234789237898970023894897023487902340237847890234780978902373890823572309742309874023987940879237902348924389792387089027702983n;
+// document.writeln(x+" Type is. : "+ typeof x+ " <br>");
+
+
+// x = undefined;
+// document.writeln(x+" Type is. : "+ typeof x+ " <br>");
+
+// x = null
+// document.writeln(x+" Type is. : "+ typeof x+ " <br>");
+
+
+// // reference types
+
+// let a = [12,23,34,45];
+// document.writeln(a+" Type is. : "+ typeof a+ " <br>");
+
+// document.writeln(a[0]);
+
+// a[4] = 100;
+// document.writeln(a[4]+" Type is. : "+ typeof a[4]+ " <br>");
+// a.push(200);
+// document.writeln(a+" Type is. : "+ typeof a+ " <br>");
+
+// a.pop();
+// document.writeln(a+" Type is. : "+ typeof a+ " <br>");
+
+
+// // objects
+
+// const person = { 
+//     'name' : "kiran",
+//     'age' : 25,
+//     'city' : "hyderabad"
+// };
+
+// document.writeln(person.name+" Type is. : "+ typeof person+ " <br>");
+
+// document.writeln(person['name']+" Type is. : "+ typeof person+ " <br>");
+
+// const person2 = { 
+//     'name' : "ravi",
+//     'age' : 25,
+//     'city' : "vijayawada"
+// };
+
+// document.writeln(person2['name']);
+
+
+
+// function
+
+function greet(name){
+    document.writeln("Hello "+name+"<br>");
 }
 
-
-document.writeln("The value of x is: " + x+ "<br>");
-
-var x = 100; //hoisting;
+greet("Ravi");
+greet("Kiran");
